1c1b307fc9aece8a698cbb2598bc4ed6d33ac3e8
amosmim <amosmim@gmail.com>
2018-05-15 11:52:01 +0300

    react native web3 exmpale

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import React, { Component } from 'react';
-import { Text } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import './global';
+const Web3 = require('web3');
 
-export default class HelloWorldApp extends Component {
+export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            text : 'blbl'
+        }
+    }
   render() {
+      const web3 = new Web3(
+          new Web3.providers.HttpProvider('http://localhost:8545')
+      );
+
+        web3.eth.getBlock('latest').then((res)=> {this.state.text = res;}).catch((res)=> {this.state.text = 'error:' + res;));
     return (
-      <Text>Hello world!</Text>
+      <View style={styles.container}>
+        <Text>OverHeader project for 2018 BIU Hackton</Text>
+        <Text>{this.state.text}</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
